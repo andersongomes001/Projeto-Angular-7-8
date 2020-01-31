edc32f1e5c26b4b28a1bad20d278254a441449ef
administrador <administrador@zaruc.com.br>
2020-01-30 23:30:39 -0300

    directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PrimeirocomponenteComponent } from './primeirocomponente/primeirocomponente.component';
 import { FormsModule } from '@angular/forms';
+import { ChangetextDirective } from './changetext.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrimeirocomponenteComponent
+    PrimeirocomponenteComponent,
+    ChangetextDirective
   ],
   imports: [
     BrowserModule,
