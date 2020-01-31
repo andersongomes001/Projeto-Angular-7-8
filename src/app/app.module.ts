a2def737f5cf76dfe029414a98b45912b1ebbbbf
administrador <administrador@zaruc.com.br>
2020-01-30 21:57:00 -0300

    Primeirro componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PrimeirocomponenteComponent } from './primeirocomponente/primeirocomponente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeirocomponenteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
